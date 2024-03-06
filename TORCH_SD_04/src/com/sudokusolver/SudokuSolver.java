42729e15551d1f6e0b01447ca0a5939e8afcfd25
dhiraj29patil <dhiraj29patil@gmail.com>
2024-03-06 11:48:06 +0530

    TORCH_SD_04

---
+++ TORCH_SD_04/src/com/sudokusolver/SudokuSolver.java
@@ -0,0 +1,83 @@
+package com.sudokusolver;
+
+public class SudokuSolver {
+    public static void main(String[] args) {
+        int[][] sudokuGrid = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        System.out.println("Sudoku puzzle Unsolved:");
+        System.out.println();
+        
+        printSudoku(sudokuGrid);
+        System.out.println();
+        
+        solveSudoku(sudokuGrid);
+        
+        System.out.println("Sudoku puzzle solved:");
+        System.out.println();
+        printSudoku(sudokuGrid);
+    }
+
+    public static boolean solveSudoku(int[][] board) {
+        int[] emptyCell = findEmptyCell(board);
+        if (emptyCell == null) {
+            return true;
+        }
+
+        int row = emptyCell[0];
+        int col = emptyCell[1];
+
+        for (int num = 1; num <= 9; num++) {
+            if (isValidMove(board, row, col, num)) {
+                board[row][col] = num;
+
+                if (solveSudoku(board)) {
+                    return true;
+                }
+
+                board[row][col] = 0;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isValidMove(int[][] board, int row, int col, int num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num || board[row - row % 3 + i / 3][col - col % 3 + i % 3] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int[] findEmptyCell(int[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == 0) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void printSudoku(int[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
